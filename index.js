83a79e777076c65a30fcda0827a8abb6329088f4
Vitaly Kramskikh <vkramskikh@apstra.com>
2023-12-16 16:05:55 +0100

    First working version

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const {createProxyMiddleware} = require('http-proxy-middleware');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
 const PORT = 3000;
 
 // Serve the local index.html file
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  fs.readFile(path.join(__dirname, 'index.html'), function(err, data) {
+    if (err) {
+        res.sendStatus(500);
+    } else {
+        res.set('Content-Type', 'text/html');
+        res.send(data.toString().replace('MAPS_API_KEY', process.env.MAPS_API_KEY));
+    }
+  });
 });
 
 app.use('*', createProxyMiddleware({target: 'https://maps.nrc.nl', changeOrigin: true}));
